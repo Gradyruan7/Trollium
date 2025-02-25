25dfebf0952855bb2b311d8967c4e9e9bf981bcc
heybroskii <Gunnarkittykatz@gmail.com>
2025-02-25 18:34:47 -0500

    Update module.js

--- src/module/module.js
+++ src/module/module.js
@@ -7,37 +7,37 @@ export default class Module {
         this.description = description;
         this.category = category;
         this.options = options;
-        this.keybind = configManager.config?.modules?.[name]?.keybind || keybind;
+        this.keybind = configManager.config?.modules?.[j]?.keybind || keybind;
         this.waitingForBind = false;
         this.isEnabled = false;
         this.toggle = this.toggle.bind(this);
     }
 
-    onEnable () {}
-    onDisable() {}
+    onEnable (j) {}
+    onDisable(k) {}
     onGameTick() {}
-    onRender() {}
-    onGameEntered() {}
-    onGameExited() {}
+    onRender(l) {}
+    onGameEntered(j) {}
+    onGameExited(k) {}
     onSettingUpdate() {}
 
-    enable () {
+    enable (j) {
         this.isEnabled = true;
         eventListener.emit("module.update", this);
         this.onEnable();
     }
 
-    disable () {
+    disable (k) {
         this.isEnabled = false;
         eventListener.emit("module.update", this);
         this.onDisable();
     }
 
-    toggle () {
+    toggle (j) {
         if (this.isEnabled) {
-            this.disable();
+            this.disable(k);
         } else {
-            this.enable();
+            this.enable(k);
         }
     };
 };
